3eea86714fb22eebd11b695a3ac4bfa28f805c27
Ritu Gala <39478730+ritugala@users.noreply.github.com>
2021-05-21 14:54:03 +0530

    Added Missing Files

---
+++ deployment.bat
@@ -0,0 +1,11 @@
+
+cd build
+git remote remove origin
+git init
+git remote add origin https://github.com/BlockEye/BlockEye.github.io.git
+git add .
+git commit -m 'changes'
+git push --force origin master
+cd..
+git remote remove origin
+git remote add origin https://github.com/ritugala/BlockEye.git
