9f34ad1d2b468bf2d49957d4bed7bd38be78a8b1
VidhiRambhia <vsrambhia@gmail.com>
2021-05-22 12:52:39 +0530

    Removing animations and what we do title

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -12,42 +12,42 @@ import FirstImage from "./FirstImage";
 export default function Greeting(props) {
   const theme = props.theme;
   return (
-    <Fade bottom duration={2000} distance="40px">
-      <div className="greet-main" id="greeting">
-        <div className="greeting-main">
-          <div className="greeting-text-div">
-            <div style={{ whiteSpace: "nowrap", overflowX: "auto" }}>
-              <div className="row">
-                <Logo />
-                <h1 className="greeting-text" style={{ color: theme.text }}>
-                  {greeting.title}
-                </h1>
-              </div>
+    // <Fade bottom duration={2000} distance="40px">
+    <div className="greet-main" id="greeting">
+      <div className="greeting-main">
+        <div className="greeting-text-div">
+          <div style={{ whiteSpace: "nowrap" }}>
+            <div className="row">
+              <Logo />
+              <h1 className="greeting-text" style={{ color: theme.text }}>
+                {greeting.title}
+              </h1>
+            </div>
 
-              <p
-                className="greeting-text-p subTitle"
-                style={{
-                  color: theme.secondaryText,
-                  fontSize: "1.5em",
-                  display: "inline-block",
-                  marginTop: "-10px",
-                }}
-              >
-                {greeting.subTitle}
-              </p>
-              <SocialMedia theme={theme} />
+            <p
+              className="greeting-text-p subTitle"
+              style={{
+                color: theme.secondaryText,
+                fontSize: "1.5em",
+                display: "inline-block",
+                marginTop: "-10px",
+              }}
+            >
+              {greeting.subTitle}
+            </p>
+            <SocialMedia theme={theme} />
 
-              {/* <div className="button-greeting-div">
+            {/* <div className="button-greeting-div">
               <Button text="Contact me" href="#contact" />
               <Button text="See my resume" newTab={true} href={greeting.resumeLink} />
             </div> */}
-            </div>
-          </div>
-          <div className="greeting-image-div">
-            <FirstImage />
           </div>
         </div>
+        <div className="greeting-image-div">
+          <FirstImage />
+        </div>
       </div>
-    </Fade>
+    </div>
+    // </Fade>
   );
 }
--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -10,12 +10,7 @@ import HumanoidImg from "./HumanoidImg";
 
 function GetSkillSvg(props) {
   if (props.fileName === "BlockchainImg")
-    return (
-      <BlockchainImg
-        theme={props.theme}
-        style={{ marginTop: "0px", marginLeft: "15%" }}
-      />
-    );
+    return <BlockchainImg theme={props.theme} />;
   else if (props.fileName === "HumanoidImg")
     return <HumanoidImg theme={props.theme} />;
   return <DesignImg theme={props.theme} />;
@@ -29,35 +24,35 @@ class SkillSection extends Component {
         {skills.data.map((skill) => {
           return (
             <div className="skills-main-div">
-              <Fade left duration={2000}>
-                <div className="skills-image-div">
-                  {/* <img
+              {/* <Fade left duration={2000}> */}
+              <div className="skills-image-div">
+                {/* <img
                     alt="Ashutosh is Analysing Data"
                     src={require(`../../assests/images/${skill.imagePath}`)}
                   ></img> */}
-                  <GetSkillSvg fileName={skill.fileName} theme={theme} />
-                </div>
-              </Fade>
+                <GetSkillSvg fileName={skill.fileName} theme={theme} />
+              </div>
+              {/* </Fade> */}
 
               <div className="skills-text-div">
-                <Fade right duration={1000}></Fade>
-                <Fade right duration={1500}>
-                  <SoftwareSkill logos={skill.softwareSkills} />
-                </Fade>
-                <Fade right duration={2000}>
-                  <div>
-                    {skill.skills.map((skillSentence) => {
-                      return (
-                        <p
-                          className="subTitle skills-text"
-                          style={{ color: theme.secondaryText }}
-                        >
-                          {skillSentence}
-                        </p>
-                      );
-                    })}
-                  </div>
-                </Fade>
+                {/* <Fade right duration={1000}></Fade>
+                <Fade right duration={1500}> */}
+                <SoftwareSkill logos={skill.softwareSkills} />
+                {/* </Fade>
+                <Fade right duration={2000}> */}
+                <div>
+                  {skill.skills.map((skillSentence) => {
+                    return (
+                      <p
+                        className="subTitle skills-text"
+                        style={{ color: theme.secondaryText }}
+                      >
+                        {skillSentence}
+                      </p>
+                    );
+                  })}
+                </div>
+                {/* </Fade> */}
               </div>
             </div>
           );
