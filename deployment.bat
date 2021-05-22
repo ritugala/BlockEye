59ef03a39a93c694754fdde5b81a42568cf4add7
Ritu Gala <39478730+ritugala@users.noreply.github.com>
2021-05-22 17:40:28 +0530

    Added CNAME

--- deployment.bat
+++ deployment.bat
@@ -4,7 +4,7 @@ git remote remove origin
 git init
 git remote add origin https://github.com/BlockEye/BlockEye.github.io.git
 git add .
-git commit -m 'changes'
+git commit -m "changes"
 git push --force origin master
 cd..
 git remote remove origin
