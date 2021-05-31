3accb0385c24bca6ffb0495c9e3f4ef7273697b2
Ritu Gala <39478730+ritugala@users.noreply.github.com>
2021-05-31 11:19:21 +0530

    Slogan Bug Fixed

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -10,9 +10,26 @@ import Logo from "../../components/header/Logo";
 import FirstImage from "./FirstImage";
 import { skills } from "../../portfolio";
 import SoftwareSkill from "../../components/softwareSkills/SoftwareSkill";
+import { ResizeObserver } from "resize-observer";
+
+function changeFontSize() {
+  var greeting_logo = document.getElementsByClassName("greeting-logo-title")[0];
+  var p_size = document.getElementsByClassName("greeting-logo-title")[0]
+    .offsetWidth;
+  document.getElementsByClassName("greeting-text-p")[0].style.fontSize =
+    (p_size * 1.0) / 500 + "em";
+  document.getElementsByClassName("dev-icons")[0].style.fontSize =
+    (p_size * 2.1) / 500 + "em";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  var greeting_logo = document.getElementsByClassName("greeting-logo-title")[0];
+  new ResizeObserver(changeFontSize).observe(greeting_logo);
+});
 
 export default function Greeting(props) {
   const theme = props.theme;
+
   return (
     // <Fade bottom duration={2000} distance="40px">
     <div className="greet-main" id="greeting">
@@ -26,14 +43,7 @@ export default function Greeting(props) {
               </h1>
             </div>
 
-            <p
-              className="greeting-text-p subTitle"
-              style={{
-                color: theme.secondaryText,
-                display: "flex",
-                marginTop: "-10px",
-              }}
-            >
+            <p className="greeting-text-p subTitle" onLoad>
               {greeting.subTitle}
             </p>
 
